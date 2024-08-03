711a4a08b0ed784bc912cd684c65e44263ccdb65
legacychimera247 <mad_fer_it@msn.com>
2024-08-03 15:50:51 +0200

    Add files via upload

---
+++ translations/settings-clockwork-it.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name></name>
+    <message id="settings_clockwork-licensing">
+        <source>%1 is distributed under terms of %2MIT License%3.</source>
+        <extracomment>%1 is application name, text surrounded by %2 and %3 is a link</extracomment>
+        <translation>%1 è distribuito sotto %2Licencia MIT%3.</translation>
+    </message>
+    <message id="settings_clockwork-report_issue">
+        <source>Report issue</source>
+        <translation>Segnala un problema</translation>
+    </message>
+    <message id="settings_clockwork-about_app">
+        <source>About %1</source>
+        <translation>A proposito di %1</translation>
+    </message>
+    <message id="settings_clockwork-dynamic_icons">
+        <source>Dynamic icons</source>
+        <translation>Icone dinamiche</translation>
+    </message>
+    <message id="settings_clockwork-icon_packs">
+        <source>Icon packs</source>
+        <translation>Pacchi di icone</translation>
+    </message>
+    <message id="settings_clockwork-icon_pack_enabled">
+        <source>In use</source>
+        <translation>In uso</translation>
+    </message>
+    <message id="settings_clockwork-not_use">
+        <source>Not use</source>
+        <translation>Non in uso</translation>
+    </message>
+    <message id="settings_clockwork-use">
+        <source>Use</source>
+        <translation>Usare</translation>
+    </message>
+    <message id="settings_clockwork-no_icon_packs_avail">
+        <source>No icon packs available</source>
+        <translation>Nessun pacco di icone disponibile</translation>
+    </message>
+    <message id="settings_clockwork-use_some_icon_pack">
+        <source>Use %1?</source>
+        <extracomment>%1 is the icon pack name</extracomment>
+        <translation>Usare %1?</translation>
+    </message>
+    <message id="settings_clockwork-customize_icons_hint">
+        <source>You can choose icon for application from any installed pack.</source>
+        <translation>Puoi scegliere un&apos;icona per un applicazione da qualsiasi pacco installato.</translation>
+    </message>
+    <message id="settings_clockwork-customize_icons">
+        <source>Customize icons</source>
+        <translation>Personalizza icone</translation>
+    </message>
+    <message id="settings_clockwork-default_icon">
+        <source>Default icon</source>
+        <translation>Icona predefinita</translation>
+    </message>
+</context>
+</TS>
